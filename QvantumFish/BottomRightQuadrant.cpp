d550f47e81e83b1ad67bb460411c186f7d5d1cb5
THMapnea <thmapneadesigner@gmail.com>
2025-10-31 22:42:42 +0100

    fixed coordinates exchange

--- QvantumFish/BottomRightQuadrant.cpp
+++ QvantumFish/BottomRightQuadrant.cpp
@@ -15,24 +15,28 @@ BottomRightQuadrant::~BottomRightQuadrant() {
 
 void BottomRightQuadrant::initialize() {
     std::cout << "BottomRightQuadrant initialized." << std::endl;
-    // TODO: Add initialization for bottom-right quadrant content
-    // Potential content: Quantum gates panel, control parameters, algorithm visualization
 }
 
+
 void BottomRightQuadrant::render(int viewportX, int viewportY, int viewportWidth, int viewportHeight) {
-    // Skip rendering if viewport dimensions are invalid
     if (viewportWidth <= 0 || viewportHeight <= 0) {
         return;
     }
 
     // Set up viewport for this quadrant
     glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
-
-    // Clear only depth buffer for this quadrant
     glClear(GL_DEPTH_BUFFER_BIT);
 
-    ImGui::SetNextWindowPos(ImVec2(viewportX, viewportY), ImGuiCond_Always);
-    ImGui::Begin("Fixed Window", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse);
+    // Convert Y coordinate for ImGui (from bottom-left to top-left coordinate system)
+    int screenHeight = ImGui::GetIO().DisplaySize.y;
+    int imguiY = screenHeight - viewportY - viewportHeight;
+
+    ImGui::SetNextWindowPos(ImVec2(viewportX, imguiY), ImGuiCond_Always);
+    ImGui::SetNextWindowSize(ImVec2(viewportWidth, viewportHeight));
+
+    ImGui::Begin("Fixed Window", nullptr,
+        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar);
     {
         ImGui::Text("Quantum State Information");
     }
